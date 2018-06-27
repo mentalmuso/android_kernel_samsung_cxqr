7faec8eaed991a4bd3bf53afcd178b6698c7b9a3
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-27 00:53:49 +0000

    drm: msm: kcal: make the userspace module param vars static and use short instead of integers
    Same reasoning as https://github.com/franciscofranco/enchilada/commit/20461e4aef0b54ea11c84223db4f59de14aabe57
    
    There's no point in using integers where a short is big enough.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- techpack/display/msm/sde/sde_hw_color_proc_v4.c
+++ techpack/display/msm/sde/sde_hw_color_proc_v4.c
@@ -2,21 +2,14 @@
 /*
  * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
  */
+#include <linux/moduleparam.h>
 #include <drm/msm_drm_pp.h>
 #include "sde_hw_color_proc_common_v4.h"
 #include "sde_hw_color_proc_v4.h"
 
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
-
-unsigned short kcal_red = 256;
-unsigned short kcal_green = 256;
-unsigned short kcal_blue = 256;
-#else
 static unsigned short kcal_red = 256;
 static unsigned short kcal_green = 256;
 static unsigned short kcal_blue = 256;
-#endif
 static unsigned short kcal_hue = 0;
 static unsigned short kcal_sat = 255;
 static unsigned short kcal_val = 255;
